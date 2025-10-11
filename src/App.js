802aeb444156b1aa6dbcc6a298894cc8be94d648
Emil <emil.yassinov@gmail.com>
2025-10-11 01:37:29 -0300

    bundling mobile friendly designs

--- src/App.js
+++ src/App.js
@@ -9,10 +9,11 @@ import Toolbar from "@mui/material/Toolbar";
 import Button from "@mui/material/Button";
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
-import {Box, Typography} from "@mui/material";
+import {Box, Drawer, IconButton, List, ListItem, ListItemButton, ListItemText, Typography} from "@mui/material";
 import PropTypes from "prop-types";
 import SnackBarWrapper from "./Helper/snackBarWrapper";
 import WAVES from "vanta/src/vanta.waves";
+import MenuIcon from '@mui/icons-material/Menu';
 
 function TabPanel(props) {
     const { children, value, index, ...other } = props;
@@ -52,6 +53,7 @@ function App() {
     const [value, setValue] = useState(0);
     const [isMobile] = useState(window.innerWidth <= 768);
     const [typingIsDone, setTypingIsDone] = useState(false);
+    const [drawerState, setDrawerState] = useState(false);
 
     const handleChange = (event, newValue) => {
         setValue(newValue);
@@ -76,6 +78,41 @@ function App() {
                 return <MainPage typingIsDone={typingIsDone} setTypingIsDone={setTypingIsDone}/>;
         }
     };
+
+    const toggleDrawer = (newOpen) => () => {
+        setDrawerState(newOpen);
+    };
+
+    const drawerList = (
+        <Box
+            sx={{
+                backgroundColor: "#3e0775",
+                height: "100%",
+                color: "white"
+            }}
+            onClick={toggleDrawer(false)}
+        >
+            <List>
+                {['Main Page', 'Algorithm Project', 'Prompted Game'].map((text, index) => (
+                    <ListItem
+                        sx={{
+                            marginBottom: 1 + 'em',
+                        }}
+                        key={text} disablePadding
+                    >
+                        <ListItemButton
+                            onClick={() => {
+                                handleChange(null, index);
+                                toggleDrawer(false);
+                            }}
+                        >
+                            <ListItemText primary={text} />
+                        </ListItemButton>
+                    </ListItem>
+                ))}
+            </List>
+        </Box>
+    );
 
     const [vantaEffect, setVantaEffect] = useState(null)
     const waveRef = useRef(null)
@@ -106,23 +143,38 @@ function App() {
                <AppBar className="default-padding" position="static" color="transparent" sx={{ backdropFilter: "blur(10px)" }}>
                     <Toolbar sx={{ justifyContent: "space-between", padding: 0}}>
                         <div style={{ display: "flex"}}>
-                            <Tabs
-                                sx={{ justifyContent: "space-between"}}
-                                value={value}
-                                onChange={handleChange}
-                                TabIndicatorProps={{
+                            {isMobile ? (
+                                <React.Fragment>
+                                    <IconButton onClick={toggleDrawer(true)}>{<MenuIcon color="secondary"/>}</IconButton>
+                                    <Drawer
+                                        open={drawerState}
+                                        onClose={toggleDrawer(false)}
+                                        sx={{
+                                            backdropFilter: "blur(3px)",
+                                        }}
+                                    >
+                                        {drawerList}
+                                    </Drawer>
+                                </React.Fragment>
+                            ) : (
+                                <Tabs
+                                    sx={{ justifyContent: "space-between"}}
+                                    value={value}
+                                    onChange={handleChange}
+                                    TabIndicatorProps={{
                                     style: {
-                                        backgroundColor: "white"
+                                    backgroundColor: "white"
                                     }
-                                }}
-                                textColor="inherit"
-                                aria-label="full width tabs"
-                                variant={isMobile ? "fullWidth" : "standard"}
-                            >
-                                <Tab label={isMobile ? "Main" : "Main page"} {...a11yProps(0)} />
-                                <Tab label={isMobile ? "Alg project" : "Algorithm Project"} {...a11yProps(1)} />
-                                <Tab label="Prompted game" {...a11yProps(1)} />
-                            </Tabs>
+                                    }}
+                                    textColor="inherit"
+                                    aria-label="full width tabs"
+                                    variant={isMobile ? "fullWidth" : "standard"}
+                                >
+                                    <Tab label={isMobile ? "Main" : "Main page"} {...a11yProps(0)} />
+                                    <Tab label={isMobile ? "Alg project" : "Algorithm Project"} {...a11yProps(1)} />
+                                    <Tab label="Prompted game" {...a11yProps(1)} />
+                                </Tabs>
+                            )}
                         </div>
                         <div style={{ display: "flex" }}>
                             <Button style={{marginRight: 1 + 'em'}} variant="outlined" href="https://www.linkedin.com/in/emil-yassinov-8aa6b21a0/" target="_blank" rel="noopener noreferrer" color="inherit">LinkedIn</Button>
